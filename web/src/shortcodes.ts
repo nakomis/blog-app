bc9f76935916f3e2cfb27e1fc9f76752819dc795
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-04-03 20:10:31 +0100

    Add hr spacing in CSS; add hr and prompt text to donate shortcode

--- web/src/shortcodes.ts
+++ web/src/shortcodes.ts
@@ -1,5 +1,7 @@
 const shortcodes: Record<string, string> = {
-  donate: `<form action="https://www.paypal.com/donate" method="post" target="_top">
+  donate: `<hr />
+<p>If you find this blog useful, please consider a donation.</p>
+<form action="https://www.paypal.com/donate" method="post" target="_top">
 <input type="hidden" name="hosted_button_id" value="Q3BESC73EWVNN" />
 <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 <img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
